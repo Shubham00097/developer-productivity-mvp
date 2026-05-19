91a73871197ea50147d595c180980e83e6b1d19d
Shubham00097 <shubham10809@gmail.com>
2026-05-20 02:51:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,8 @@ const metricsRoute = require("./routes/metrics");
 const managerRoute = require("./routes/manager");
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/metrics", metricsRoute);
